3c94f16d4108898a662f47b4acf06d57e8b72ffa
Lachlan Miller <lachlanmiller@Lachlans-MBP.modem>
2022-11-24 09:34:29 +1000

    basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,58 @@
 import { useState } from "react";
-import * as fuzzy from './fuzzysort'
-import type { DirectoryNode, FileNode } from "./tree";
+import { DirectoryNode, FileNode, getPath } from "./tree";
 import { deriveTree } from "./tree";
-import "./App.css";
 
 const indent = 15;
+
+type FileClickHandler = (file: FileNode) => void;
+
+type FolderClickHander = (folder: DirectoryNode) => void;
 
 const Directory: React.FC<{
   directory: DirectoryNode;
   depth: number;
+  onFolderClick: FolderClickHander;
+  onFileClick: FileClickHandler;
 }> = (props) => {
   return (
     <>
-      <div style={{ marginLeft: `${props.depth * indent}px` }}>
-        {props.directory.displayName
-          .split("")
-          .map((char, idx) =>
-            props.directory.highlight.includes(idx) ? <b>{char}</b> : char
-          )}
+      <div
+        style={{ marginLeft: `${props.depth * indent}px` }}
+        onClick={() => props.onFolderClick(props.directory)}
+      >
+        <div>
+          {props.directory.displayName
+            .split("")
+            .map((char, idx) =>
+              props.directory.highlight.includes(idx) ? (
+                <b key={idx}>{char}</b>
+              ) : (
+                char
+              )
+            )}
+        </div>
       </div>
 
-      {props.directory.directories.map((directory) => (
-        <Directory
-          directory={directory}
-          key={directory.relative}
-          depth={props.depth + 1}
-        />
-      ))}
+      {props.directory.expanded &&
+        props.directory.directories.map((directory) => (
+          <Directory
+            directory={directory}
+            key={directory.relative}
+            depth={props.depth + 1}
+            onFolderClick={props.onFolderClick}
+            onFileClick={props.onFileClick}
+          />
+        ))}
 
-      {props.directory.files.map((file) => (
-        <File file={file} key={file.relative} depth={props.depth + 1} />
-      ))}
+      {props.directory.expanded &&
+        props.directory.files.map((file) => (
+          <File
+            file={file}
+            key={file.relative}
+            depth={props.depth + 1}
+            onFileClick={props.onFileClick}
+          />
+        ))}
     </>
   );
 };
@@ -38,43 +60,71 @@ const Directory: React.FC<{
 const File: React.FC<{
   file: FileNode;
   depth: number;
+  onFileClick: (file: FileNode) => void;
 }> = (props) => {
   return (
-    <div style={{ marginLeft: `${props.depth * indent + 0}px` }}>
+    <div
+      style={{ marginLeft: `${props.depth * indent + 0}px` }}
+      onClick={() => props.onFileClick(props.file)}
+    >
       {props.file.displayName
         .split("")
         .map((char, idx) =>
-          props.file.highlight.includes(idx) ? <b>{char}</b> : char
+          props.file.highlight.includes(idx) ? <b key={idx}>{char}</b> : char
         )}
     </div>
   );
 };
 
-interface Options {
-  search: string;
-}
-
 function App() {
-  window.fuzzy = fuzzy
   const files = [
     "foo/bar/merp.js",
     "foo/bar/baz.js",
     "foo/bar/baz/merp/lux/qux.js",
     "foo/foo.js",
   ];
-  window.files = files
 
   const [search, setSearch] = useState("");
-  const [tree, setTree] = useState(deriveTree(files));
+  const [collapsed, setCollapsed] = useState(new Set<string>());
+  const [tree, setTree] = useState(deriveTree(files, { collapsed }));
 
   function onSearch(search: string) {
     setSearch(search);
-    setTree(deriveTree(files));
+    const c = new Set<string>();
+    setCollapsed(c);
+    setTree(deriveTree(files, { search, collapsed: c }));
   }
 
-  return tree.directories.map((item) => (
-    <Directory directory={item} key={item.relative} depth={0} />
-  ));
+  const onFolderClick: FolderClickHander = (folder) => {
+    const rel = getPath(folder.relative);
+    const c = new Set([...collapsed]);
+    if (c.has(rel)) {
+      c.delete(rel);
+    } else {
+      c.add(rel);
+    }
+    setCollapsed(c);
+    setTree(deriveTree(files, { search, collapsed: c }));
+  };
+
+  const onFileClick: FileClickHandler = (file) => {
+    console.log(`Clicked`, file);
+  };
+
+  return (
+    <div>
+      <input value={search} onInput={(e) => onSearch(e.currentTarget.value)} />
+      {tree.directories.map((item) => (
+        <Directory
+          directory={item}
+          key={item.relative}
+          depth={0}
+          onFolderClick={onFolderClick}
+          onFileClick={onFileClick}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/tree.ts
+++ src/tree.ts
@@ -1,16 +1,37 @@
-// interface TreeOptions {
-//   noCircularDeps: boolean;
-// }
+import { fuzzysort } from "./fuzzysort";
 
 interface Entry {
   name: string;
   relative: string;
   originalRelative: string;
+  indexes: number[];
   parent: string;
   files: Array<{
     name: string;
-    relative: string
-  }>
+    relative: string;
+    indexes: number[];
+  }>;
+}
+
+export interface Item {
+  displayName: string;
+  highlight: number[];
+  relative: string;
+}
+
+export type FileNode = Item;
+
+interface InternalDirectoryNode extends Item {
+  directories: DirectoryNode[];
+  files: Item[];
+  parent?: InternalDirectoryNode;
+  expanded: boolean;
+}
+
+export type DirectoryNode = Omit<InternalDirectoryNode, "parent">;
+
+export interface Tree {
+  directories: DirectoryNode[];
 }
 
 function longestCommonPath(a: string[], b: string[]): string[] {
@@ -29,14 +50,14 @@ function longestCommonPath(a: string[], b: string[]): string[] {
 function collapse(
   curr: string[],
   dir: string[],
-  target: string,
-  knownDirs: Array<string[]>
+  target: FuzzyFile,
+  knownDirs: Array<{ relative: string[]; indexes: number[] }>
 ): Entry {
   let longest: string[] = [];
 
   for (let i = 0; i < knownDirs.length; i++) {
     const curr = knownDirs[i];
-    const next = longestCommonPath(curr, dir);
+    const next = longestCommonPath(curr.relative, dir);
     if (next.length > longest.length) {
       longest = next;
     }
@@ -48,21 +69,29 @@ function collapse(
   const name = trun.slice(0, trun.length - 1).join("/");
   const file = curr.slice(-1)[0];
 
+  // need to adjust indexes
+  const adjusted = adjustIndexes(file, target, "lastIndexOf");
+  const pathAdjusted = adjustIndexes(name, target, "indexOf");
+
   const e: Entry = {
     relative,
     name,
+    indexes: pathAdjusted,
     parent,
-    originalRelative: target,
-    files: [{
-      name: file,
-      relative: curr.join('/')
-  }],
+    originalRelative: target.relative,
+    files: [
+      {
+        name: file,
+        relative: curr.join("/"),
+        indexes: adjusted,
+      },
+    ],
   };
 
   return e;
 }
 
-function getPath(file: string) {
+export function getPath(file: string) {
   const s = file.split("/");
   return s.slice(0, s.length - 1).join("/");
 }
@@ -72,25 +101,48 @@ function getFilename(file: string) {
   return s.slice(s.length - 1)[0];
 }
 
+function adjustIndexes(
+  file: string,
+  target: FuzzyFile,
+  pos: "indexOf" | "lastIndexOf"
+) {
+  // need to adjust indexes
+  const startsAt = target.relative[pos](file);
+  return target.indexes
+    .filter((idx) => idx >= startsAt && idx <= startsAt + file.length)
+    .map((x) => x - startsAt);
+}
+
 function makeEntry(
-  target: string,
-  files: string[],
-  knownDirs: Array<string[]>,
+  target: FuzzyFile,
+  files: FuzzyFile[],
+  knownDirs: Array<{ relative: string[]; indexes: number[] }>,
   entries: Entry[]
 ): { best?: string[]; entry: Entry } {
-  const knownSibling = entries.find((x) => x.relative === getPath(target));
+  const knownSibling = entries.find(
+    (x) => x.relative === getPath(target.relative)
+  );
 
   if (knownSibling) {
+    const file = getFilename(target.relative);
+    const adjusted = adjustIndexes(file, target, "lastIndexOf");
+
     return {
       entry: {
         ...knownSibling,
-        files: knownSibling.files.concat({ name: getFilename(target), relative: target }),
+        files: knownSibling.files.concat({
+          name: file,
+          indexes: adjusted,
+          relative: target.relative,
+        }),
       },
     };
   }
 
-  const others = files.filter((x) => x !== target).map((x) => x.split(`/`));
-  const curr = target.split("/");
+  const others = files
+    .filter((x) => x !== target)
+    .map((x) => x.relative.split(`/`));
+  const curr = target.relative.split("/");
 
   let best: string[] = [];
   for (let i = 0; i < others.length; i++) {
@@ -107,44 +159,48 @@ function makeEntry(
   };
 }
 
-export interface Item {
-  displayName: string;
-  highlight: number[];
+interface FuzzyFile {
+  /** This is the filename (includes relative path) */
   relative: string;
+  /** fuzzy indexes */
+  indexes: number[];
 }
 
-export type FileNode = Item
-
-interface InternalDirectoryNode extends Item {
-  directories: DirectoryNode[];
-  files: Item[];
-  parent?: InternalDirectoryNode;
+function sortByRelative<T extends { relative: string }>(x: T, y: T): -1 | 1 {
+  return x.relative.split("/").length < y.relative.split("/").length ? -1 : 1;
 }
 
-export type DirectoryNode = Omit<InternalDirectoryNode, "parent">;
-
-export interface Tree {
-  directories: DirectoryNode[];
+interface TreeOptions {
+  search: string;
+  collapsed: Set<string>;
 }
 
-export function deriveTree(files: string[]): DirectoryNode {
-  files.sort((x, y) => (x.split("/").length < y.split("/").length ? -1 : 1));
+export function deriveTree(files: string[], options: Partial<TreeOptions> = {}): DirectoryNode {
+  const filtered: FuzzyFile[] = (options.search ? fuzzysort(options.search, files) : files)
+    .map((x) => ({
+      indexes: (x as any).indexes ?? [],
+      relative: (x as any).target ?? x,
+    }))
+    .sort(sortByRelative);
 
   const entries: Entry[] = [];
-  const dirs: Array<string[]> = [];
+  const dirs: Array<{ relative: string[]; indexes: number[] }> = [];
   const map = new Map<string, Entry>();
 
-  for (const file of files) {
-    const { entry, best } = makeEntry(file, files, dirs, entries);
+  for (const file of filtered) {
+    const { entry, best } = makeEntry(file, filtered, dirs, entries);
     entries.push(entry);
     if (best) {
-      dirs.push(best);
+      const joined = best.join("/");
+      const indexes = adjustIndexes(joined, file, "indexOf");
+      dirs.push({ relative: best, indexes });
     }
     map.set(entry.relative, entry);
   }
 
   const root: InternalDirectoryNode = {
     displayName: "/",
+    expanded: true,
     parent: undefined,
     highlight: [],
     relative: "/",
@@ -157,17 +213,19 @@ export function deriveTree(files: string[]): DirectoryNode {
   for (const entry of entries) {
     const parent =
       entry.relative === entry.name ? root : dirmap.get(entry.parent)!;
+
     dirmap.set(entry.relative, {
+      expanded: !options.collapsed?.has(getPath(entry.originalRelative)),
       displayName: entry.name,
       parent,
-      highlight: [],
+      highlight: entry.indexes,
       relative: entry.originalRelative,
       files: entry.files
         .map((x) => {
           return {
             relative: x.relative,
             displayName: x.name,
-            highlight: [],
+            highlight: x.indexes,
           };
         })
         .sort((x, y) => {
